63fd7011a2e4a6b63b97a98e6cc1d79b43834512
Daniyar <daniyar.kuttymbek@gmail.com>
2020-04-24 18:02:48 +0600

    v2

--- Kuttymbek Daniyar/gmp.cpp
+++ Kuttymbek Daniyar/gmp.cpp
@@ -3,28 +3,11 @@
 #include <gmp.h>
 
 using namespace std;
-mpz_t z;
-
-
 
 int main() {
-    mpz_t x,y;
-    mpz_init(x);
-    mpz_init(y);
-    mpz_init(z);
-
-    gmp_scanf("%Zd%Zd",x,y);
-    if (mpz_cmp(x,y) == -1){
-         mpz_swap(x,y);
-    }
-    while(mpz_sgn(y)){
-        mpz_mod(z,x,y);
-        mpz_set(x,y);
-        mpz_set(y,z);
-    }
-    gmp_printf("%Zd",x);
-    mpz_clear(x);
-    mpz_clear(y);
-    mpz_clear(z);
-    return 0;
+    mpz_class x,y,z;
+    cin >> x >> y;
+    if(x < y) swap(x,y);
+    while(y) z=y,y=x%y,x=z;
+    cout << x;
 }
